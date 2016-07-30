ce2b64e94ccd1de8845b0c8195bf5930a0bfdb19
RobWillison <Rob@willison.co.uk>
2016-07-30 19:50:24 +0100

    init basically

---
+++ module/HallsApplication/src/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HallsApplication;
+
+class Module
+{
+    const VERSION = '1.0dev';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
